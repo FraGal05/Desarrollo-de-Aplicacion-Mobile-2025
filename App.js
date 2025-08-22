073c49abc448269878319de65440f62b91a38ff2
FraGal05 <46552826@roquegonzalez.org>
2025-08-22 20:02:08 -0300

    primer commit de la app Login

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.form}>
+        <Text style={styles.titleText}>Login</Text>
+        <TextInput style={styles.input} keyboardType='email-address' placeholder='email address'></TextInput>
+        <TextInput style={styles.input} placeholder='password'></TextInput>
+        <Button title='Login'/>
+      </View>
     </View>
   );
 }
@@ -17,4 +21,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    margin: 30,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  form: {
+    height: 350,
+    padding: 10,
+    width: 250,
+    borderWidth:1,
+    borderColor: "#808080",
+    borderRadius: 15,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  input: {
+    margin: 15,
+    padding: 10,
+    borderWidth: 1,
+    height: 40,
+    width: 200,
+    borderColor: "#808080",
+    backgroundColor: "#fff",
+    borderRadius: 15,
+  }
 });
