d5e504fc7e9de25182e0097d858b03c6ada6a15a
FraGal05 <46552826@roquegonzalez.org>
2025-09-24 19:50:02 -0300

    clase 7 dia 24/9/2025

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,10 +1,5 @@
-import { Stack } from "expo-router"
+import { Drawer } from 'expo-router/drawer';
 
-export default function RootLayout () {
-    return (
-        <Stack>
-            <Stack.Screen name = "(tabs)" options={{ headerShown: false}}/>
-        </Stack>
-        
-    )
+export default function Layout() {
+  return <Drawer />;
 }
